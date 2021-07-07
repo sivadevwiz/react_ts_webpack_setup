90b6656a742b5f110f5226463950917dce8052d3
SivaDevWiz <siva.devwiz@gmail.com>
2021-07-07 14:05:42 +0530

    adding react refresh plugin

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import "./styles.css";
 
 import IMAGE from "./MHN_video.png";
 import LOGO from "./react.svg";
+import ClickCounter from "./ClickCounter";
 
 export const App = () => {
   return (
     <>
-      <h1>First react template - {process.env.NODE_ENV} - {process.env.name}</h1>
+      <h1>First  react template - {process.env.NODE_ENV} - {process.env.name}</h1>
       <img src={IMAGE} alt="Test logo" width="300" height="300" />
       <img src={LOGO} alt="Test logo" width="300" />
+      <ClickCounter />
+      
+      
     </>
   );
 };
