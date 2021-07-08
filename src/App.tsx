35647b596622540350cf5fab0e5554222b9f043a
SivaDevWiz <siva.devwiz@gmail.com>
2021-07-08 10:11:34 +0530

    adding ESlint for project

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import LOGO from "./react.svg";
 import ClickCounter from "./ClickCounter";
 
 export const App = () => {
+  const test = 1;
+  
   return (
     <>
       <h1>First  react template - {process.env.NODE_ENV} - {process.env.name}</h1>
       <img src={IMAGE} alt="Test logo" width="300" height="300" />
-      <img src={LOGO} alt="Test logo" width="300" />
+      <img src={LOGO} alt="Test logo" width="300"  />
       <ClickCounter />
       
       
