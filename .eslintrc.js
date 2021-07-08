9217e2e6b77ebcaa438686bdab4b7373548744bb
SivaDevWiz <siva.devwiz@gmail.com>
2021-07-08 10:21:10 +0530

    adding the ES lint fix for TSX files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,45 @@
+/*
+In case the ES lint does not work on any of the file types. 
+Add the below setting in ESLint extension's setting.
+
+"eslint.validate": [
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+    ]
+*/
+
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    extends: [// adds rules from plugins
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'plugin:jsx-a11y/recommended',
-      'plugin:eslint-comments/recommended',
-    ],
-    rules: { // custom rules apart from the rules from plugin
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error'],
-      '@typescript-eslint/no-var-requires': 'off',
-      'react/prop-types': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-    },
-  }
+  },
+  extends: [
+    // adds rules from plugins
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:eslint-comments/recommended",
+  ],
+  rules: {
+    // custom rules apart from the rules from plugin
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/no-var-requires": "off",
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
