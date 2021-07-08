17e917435ac45222ac46a754c4c8b6195bde3ee6
SivaDevWiz <siva.devwiz@gmail.com>
2021-07-08 12:04:15 +0530

    adding prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import "./styles.css";
+import './styles.css';
 
-import IMAGE from "./MHN_video.png";
-import LOGO from "./react.svg";
-import ClickCounter from "./ClickCounter";
+import IMAGE from './MHN_video.png';
+import LOGO from './react.svg';
+import ClickCounter from './ClickCounter';
 
 export const App = () => {
-  const test = 1;
-  
   return (
     <>
-      <h1>First  react template - {process.env.NODE_ENV} - {process.env.name}</h1>
+      <h1>
+        First react template - {process.env.NODE_ENV} - {process.env.name}
+      </h1>
       <img src={IMAGE} alt="Test logo" width="300" height="300" />
-      <img src={LOGO} alt="Test logo" width="300"  />
+      <img src={LOGO} alt="Test logo" width="300" />
       <ClickCounter />
-      
-      
     </>
   );
 };
